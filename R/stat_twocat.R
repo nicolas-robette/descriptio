5bf4e9a02353c6120a3c3015d2dcedd43a0521d2
Nicolas <nicolas.robette@free.fr>
2023-02-10 22:56:06 +0100

    bug fix in stat_twocat

--- R/stat_twocat.R
+++ R/stat_twocat.R
@@ -10,30 +10,31 @@ StatTwocat <- ggplot2::ggproto(
   # extra_params = c("na.rm"),
   
   compute_panel = function(self, data, scales) {
+    
     if (is.null(data$weight)) {
       data$weight <- rep(1, nrow(data))
     }
     
-    # compute cross statistics
-    panel <- with(data, assoc.twocat(x, y, weight)$gather)
+    # compute crosstabulation statistics
+    panel <- with(data, assoc.twocat(x, y, weights = weight)$gather)
     names(panel)[1:2] <- c("y","x")
-    
+
     # to handle the fact that ggplot2 could transform factors into integers
     # before computation of the statistic
     if (is.numeric(data$x)) panel$x <- as.numeric(panel$x)
     if (is.numeric(data$y)) panel$y <- as.numeric(panel$y)
     
     # keeping first value of other aesthetics in data
-    panel <- merge(
-      panel,
-      # dplyr::select(data, -dplyr::all_of("PANEL")),
-      data,
-      by = c("x", "y"),
-      all.x = TRUE
-    )
+    # panel <- merge(
+    #   panel,
+    #   # dplyr::select(data, -dplyr::all_of("PANEL")),
+    #   data,
+    #   by = c("x", "y"),
+    #   all.x = TRUE
+    # )
     
-    dups <- duplicated(panel[,c("x","y")])
-    panel <- panel[!dups,]
+    # dups <- duplicated(panel[,c("x","y")])
+    # panel <- panel[!dups,]
     # panel <- panel %>% dplyr::distinct(.data$x, .data$y, .keep_all = TRUE)
     
     # if (!keep.zero.cells) {
