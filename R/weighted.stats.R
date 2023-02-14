487acf0688249c7b6c775f5a503e6b0978c74aab
Nicolas <nicolas.robette@free.fr>
2023-02-14 22:04:49 +0100

    NA handling in weights stats functions

--- R/weighted.stats.R
+++ R/weighted.stats.R
@@ -17,14 +17,19 @@
 # https://stackoverflow.com/questions/2748725/is-there-a-weighted-median-function
 
 
-weighted.quantile <- function(x, weights, probs = seq(0, 1, 0.25),
-                              na.rm = TRUE, names = FALSE) {
+weighted.quantile <- function(x, weights = NULL, probs = seq(0, 1, 0.25),
+                              na.rm = FALSE, names = FALSE) {
   if (any(probs > 1) | any(probs < 0)) stop("probs are outside [0,1]")
-  if (length(weights) == 1) weights <- rep(weights, length(x))
-  if (length(weights) != length(x)) stop("weights must have length 1 or be as long as x")
-  if (isTRUE(na.rm)) {
-    weights <- weights[!is.na(x)]
-    x <- x[!is.na(x)]
+  # if (length(weights) == 1) weights <- rep(weights, length(x))
+  # if (length(weights) != length(x)) stop("weights must have length 1 or be as long as x")
+  if(is.null(weights)) weights <- rep(1, length(x))
+  if(any(is.na(weights))) stop("There are empty values in weights.")
+  if(na.rm) {
+    complete <- !is.na(x)
+    x <- x[complete]
+    weights <- weights[complete]
+  } else {
+    if(any(is.na(x))) stop("There are empty values in x. \nPlease consider transforming your data (filtering, recoding, imputation, etc.) or set na.rm to TRUE.")
   }
   weights <- weights[order(x)] / sum(weights)
   x <- x[order(x)]
@@ -35,7 +40,16 @@ weighted.quantile <- function(x, weights, probs = seq(0, 1, 0.25),
 }
 
 
-weighted.mad <- function(x, weights) {
+weighted.mad <- function(x, weights = NULL, na.rm = FALSE) {
+  if(is.null(weights)) weights <- rep(1, length(x))
+  if(any(is.na(weights))) stop("There are empty values in weights.")
+  if(na.rm) {
+    complete <- !is.na(x)
+    x <- x[complete]
+    weights <- weights[complete]
+  } else {
+    if(any(is.na(x))) stop("There are empty values in x. \nPlease consider transforming your data (filtering, recoding, imputation, etc.) or set na.rm to TRUE.")
+  }
   med <- weighted.quantile(x=x, weights=weights, probs = .5)
   ad <- abs(x-med)
   mad <- weighted.quantile(x=ad, weights=weights, probs = .5)
@@ -43,7 +57,16 @@ weighted.mad <- function(x, weights) {
 }
 
 
-weighted.sd <- function(x, weights) {
+weighted.sd <- function(x, weights = NULL, na.rm = FALSE) {
+  if(is.null(weights)) weights <- rep(1, length(x))
+  if(any(is.na(weights))) stop("There are empty values in weights.")
+  if(na.rm) {
+    complete <- !is.na(x)
+    x <- x[complete]
+    weights <- weights[complete]
+  } else {
+    if(any(is.na(x))) stop("There are empty values in x. \nPlease consider transforming your data (filtering, recoding, imputation, etc.) or set na.rm to TRUE.")
+  }
   mx <- stats::weighted.mean(x, weights)
   var <- stats::weighted.mean((x-mx)^2, weights)
   sd <- sqrt(var)
@@ -107,17 +130,17 @@ weighted.ktau <- function(x, y, weights) {
   return(ktau)
 }
 
-# x <- c(1,NA,4,12,3,3,6)
-# y <- c(34,87,35,12,23,98,NA)
-# weights <- c(NA,1.5,2,1.5,1,1.5,2)
 
-
-weighted.cor <- function(x, y, weights, method = "pearson", na.rm = TRUE) {
+weighted.cor <- function(x, y, weights = NULL, method = "pearson", na.rm = FALSE) {
+  if(is.null(weights)) weights <- rep(1, length(x))
+  if(any(is.na(weights))) stop("There are empty values in weights.")
   if(na.rm) {
-    complete <- !(is.na(x) | is.na(y) | is.na(weights))
+    complete <- !(is.na(x) | is.na(y))
     x <- x[complete]
     y <- y[complete]
     weights <- weights[complete]
+  } else {
+    if(any(is.na(x) | is.na(y))) stop("There are empty values in x, y or both. \nPlease consider transforming your data (filtering, recoding, imputation, etc.) or set na.rm to TRUE.")
   }
   if(method=="pearson") { 
     res <- weighted.prho(x,y,weights)
@@ -128,3 +151,39 @@ weighted.cor <- function(x, y, weights, method = "pearson", na.rm = TRUE) {
   }
   return(res)
 }
+
+
+# x0 <- c(1,3,4,12,3,3,6)
+# y0 <- c(34,87,35,12,23,98,56)
+# w0 <- c(2,1.5,2,1.5,1,1.5,2)
+# x1 <- c(1,NA,4,12,3,3,6)
+# y1 <- c(34,87,35,12,23,98,NA)
+# w1 <- c(NA,1.5,2,1.5,1,1.5,2)
+# weighted.cor(x0, y0, w0, na.rm = FALSE)
+# weighted.cor(x0, y0, w0, na.rm = TRUE)
+# weighted.cor(x0, y0, w1, na.rm = FALSE)
+# weighted.cor(x0, y0, w1, na.rm = TRUE)
+# weighted.cor(x1, y0, w0, na.rm = FALSE)
+# weighted.cor(x1, y0, w0, na.rm = TRUE)
+# weighted.cor(x0, y1, w0, na.rm = FALSE)
+# weighted.cor(x0, y1, w0, na.rm = TRUE)
+# weighted.cor(x1, y1, w0, na.rm = FALSE)
+# weighted.cor(x1, y1, w0, na.rm = TRUE)
+# weighted.sd(x0, w0, na.rm = FALSE)
+# weighted.sd(x0, w0, na.rm = TRUE)
+# weighted.sd(x0, w1, na.rm = FALSE)
+# weighted.sd(x0, w1, na.rm = TRUE)
+# weighted.sd(x1, w0, na.rm = FALSE)
+# weighted.sd(x1, w0, na.rm = TRUE)
+# weighted.quantile(x0, w0, na.rm = FALSE)
+# weighted.quantile(x0, w0, na.rm = TRUE)
+# weighted.quantile(x0, w1, na.rm = FALSE)
+# weighted.quantile(x0, w1, na.rm = TRUE)
+# weighted.quantile(x1, w0, na.rm = FALSE)
+# weighted.quantile(x1, w0, na.rm = TRUE, names = TRUE)
+# weighted.mad(x0, w0, na.rm = FALSE)
+# weighted.mad(x0, w0, na.rm = TRUE)
+# weighted.mad(x0, w1, na.rm = FALSE)
+# weighted.mad(x0, w1, na.rm = TRUE)
+# weighted.mad(x1, w0, na.rm = FALSE)
+# weighted.mad(x1, w0, na.rm = TRUE)
