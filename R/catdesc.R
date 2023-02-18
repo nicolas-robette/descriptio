7a0448170f5dfcc9ee6530c4f4f6384ad7cee296
Nicolas <nicolas.robette@free.fr>
2023-02-18 14:21:38 +0100

    Handling NA values in catdesc()

--- R/catdesc.R
+++ R/catdesc.R
@@ -1,5 +1,19 @@
-catdesc <- function(y,x,weights=rep(1,length(y)),min.phi=NULL,robust=TRUE,nperm=NULL,distrib="asympt",dec=c(3,3,3,3,1,3)) {
+catdesc <- function(y, x, weights = NULL,
+                    na.rm.cat = FALSE, na.value.cat = "NA", na.rm.cont = FALSE,
+                    min.phi = NULL, robust = TRUE,
+                    nperm = NULL, distrib = "asympt", dec = c(3,3,3,3,1,3)) {
   
+  if(is.null(weights)) weights <- rep(1, length(y))
+  if(any(is.na(weights))) stop("There are empty values in weights.")
+  
+  if(na.rm.cat==FALSE) {
+    if(any(is.na(y))) {
+      y <- factor(y, levels=c(levels(y), na.value.cat))
+      y[is.na(y)] <- na.value.cat
+      y <- factor(y)
+    }
+  }
+
   icat <- which(sapply(x,is.factor))
   xcat <- as.data.frame(x[,icat])
   names(xcat) <- names(x)[icat]
@@ -15,7 +29,7 @@ catdesc <- function(y,x,weights=rep(1,length(y)),min.phi=NULL,robust=TRUE,nperm=
   if(ncol(xcat)>0) {
     lcat <- list()
     for(i in 1:ncol(xcat)) {
-      temp <- assoc.twocat(y, xcat[,i], weights=weights, nperm=NULL)$gather
+      temp <- assoc.twocat(y, xcat[,i], weights = weights, na.rm = na.rm.cat, na.value = na.value.cat, nperm = NULL)$gather
       temp$prop.x <- NULL
       temp$prop.y <- NULL
       temp$categories <- paste(names(xcat)[i],temp$var.y,sep='.')
@@ -24,10 +38,10 @@ catdesc <- function(y,x,weights=rep(1,length(y)),min.phi=NULL,robust=TRUE,nperm=
     lcat <- do.call("rbind.data.frame",lcat)
     lcat <- lcat[order(-lcat$phi),]
     if(!is.null(min.phi)) lcat <- lcat[abs(lcat$phi)>=min.phi,]
-    lcat$cprop <- round(lcat$cprop,dec[3])
-    lcat$rprop <- round(lcat$rprop,dec[3])
-    lcat$prop.y <- round(lcat$prop.y,dec[3])
-    lcat$phi <- round(lcat$phi,dec[4])
+    lcat$cprop <- round(lcat$cprop, dec[3])
+    lcat$rprop <- round(lcat$rprop, dec[3])
+    lcat$prop.y <- round(lcat$prop.y, dec[3])
+    lcat$phi <- round(lcat$phi, dec[4])
     splitvar <- lcat$var.x
     lcat <- lcat[,c("categories","cprop","rprop","prop.y","phi")]
     names(lcat) <- c("categories","pct.ycat.in.xcat","pct.xcat.in.ycat","pct.xcat.global","phi")
@@ -43,20 +57,23 @@ catdesc <- function(y,x,weights=rep(1,length(y)),min.phi=NULL,robust=TRUE,nperm=
   if(ncol(xcon)>0) {
     lcon <- list()
     for(i in 1:ncol(xcon)) {
-      temp <- data.frame(cor = assoc.catcont(y, xcon[,i], weights=weights, nperm=NULL, digits=9)$cor)
+      temp <- data.frame(cor = assoc.catcont(y, xcon[,i], weights = weights,
+                                             na.rm.cat = na.rm.cat, na.value.cat = na.value.cat, na.rm.cont = na.rm.cont,
+                                             nperm = NULL, digits = 9)$cor)
       temp$variables <- rep(names(xcon)[i],nrow(temp))
       temp$categories <- rownames(temp)
       if(robust==TRUE) {
-        temp$median.x.in.ycat <- sapply(levels(y), function(x) weighted.quantile(xcon[y==x,i], weights[y==x], probs = .5))
-        temp$median.x.global <- rep(weighted.quantile(xcon[,i], weights, probs = .5),nrow(temp))
-        temp$mad.x.in.ycat <- sapply(levels(y), function(x) weighted.mad(xcon[y==x,i], weights[y==x]))
-        temp$mad.x.global <- rep(weighted.mad(xcon[,i], weights),nrow(temp))
+        temp$median.x.in.ycat <- sapply(levels(y), function(x) weighted.quantile(xcon[y[!is.na(y)]==x,i], weights[y[!is.na(y)]==x], 
+                                                                                 na.rm = na.rm.cont, probs = .5))
+        temp$median.x.global <- rep(weighted.quantile(xcon[,i], weights, na.rm = na.rm.cont, probs = .5), nrow(temp))
+        temp$mad.x.in.ycat <- sapply(levels(y), function(x) weighted.mad(xcon[y[!is.na(y)]==x,i], weights[y[!is.na(y)]==x], na.rm = na.rm.cont))
+        temp$mad.x.global <- rep(weighted.mad(xcon[,i], weights, na.rm = na.rm.cont), nrow(temp))
       }
       if(robust==FALSE) {
-        temp$median.x.in.ycat <- sapply(levels(y), function(x) weighted.mean(xcon[y==x,i], weights[y==x]))
-        temp$median.x.global <- rep(weighted.mean(xcon[,i], weights),nrow(temp))
-        temp$mad.x.in.ycat <- sapply(levels(y), function(x) weighted.sd(xcon[y==x,i], weights[y==x]))
-        temp$mad.x.global <- rep(weighted.sd(xcon[,i], weights),nrow(temp))       
+        temp$median.x.in.ycat <- sapply(levels(y), function(x) weighted.mean(xcon[y[!is.na(y)]==x,i], weights[y[!is.na(y)]==x], na.rm = na.rm.cont))
+        temp$median.x.global <- rep(weighted.mean(xcon[,i], weights, na.rm = na.rm.cont), nrow(temp))
+        temp$mad.x.in.ycat <- sapply(levels(y), function(x) weighted.sd(xcon[y[!is.na(y)]==x,i], weights[y[!is.na(y)]==x], na.rm = na.rm.cont))
+        temp$mad.x.global <- rep(weighted.sd(xcon[,i], weights, na.rm = na.rm.cont), nrow(temp))       
       }
       lcon[[i]] <- temp
     }
@@ -81,10 +98,33 @@ catdesc <- function(y,x,weights=rep(1,length(y)),min.phi=NULL,robust=TRUE,nperm=
     bylevel[[i]]$continuous.var <- lcon[[i]]
   }
   
-  res <- list(variables=assoc.yx(y,x,weights=weights,xx=FALSE,nperm=nperm,distrib=distrib,dec=dec[1:2])$YX,
-              bylevel=bylevel)
+  res <- list(variables = assoc.yx(y, x, weights = weights, 
+                                   na.rm.cat = na.rm.cat, na.value.cat = na.value.cat, na.rm.cont = na.rm.cont,
+                                   xx = FALSE, nperm = nperm, distrib = distrib, dec = dec[1:2])$YX,
+              bylevel = bylevel)
   return(res)
 }
 
 # data(Movies)
 # catdesc(Movies$ArtHouse, Movies[,c("Budget","Genre","Country")])
+
+# data(Movies)
+# y0 = y1 = Movies$ArtHouse
+# a0 = a1 = Movies$Budget
+# b0 = b1 = Movies$Genre
+# c0 = c1 = Movies$Country
+# w0 = w1 = Movies$Critics
+# w1[1:2] <- NA
+# a1[3:4] <- NA
+# b1[5:6] <- NA
+# c1[7:8] <- NA
+# y1[9:10] <- NA
+# 
+# catdesc(y0, data.frame(a0,b0,c0), weights = w0)
+# catdesc(y0, data.frame(a0,b0,c0), weights = w1)
+# catdesc(y0, data.frame(a1,b0,c0), na.rm.cont = FALSE)
+# catdesc(y0, data.frame(a1,b0,c0), na.rm.cont = TRUE)
+# catdesc(y0, data.frame(a0,b1,c0), na.rm.cat = FALSE, na.value.cat = "99")
+# catdesc(y0, data.frame(a0,b1,c0), na.rm.cat = TRUE, na.value.cat = "99")
+# catdesc(y1, data.frame(a0,b0,c0), na.rm.cat = FALSE, na.value.cat = "99")
+# catdesc(y1, data.frame(a0,b0,c0), na.rm.cat = TRUE, na.value.cat = "99")
