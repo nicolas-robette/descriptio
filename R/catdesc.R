813ccf60a31818250559b63c666083d85f39fbef
Nicolas <nicolas.robette@free.fr>
2023-10-04 22:43:29 +0200

    changes in catdesc()

--- R/catdesc.R
+++ R/catdesc.R
@@ -1,7 +1,24 @@
+# data(Movies)
+# dim(Movies)
+# y = Movies$Country
+# x = Movies[,-3]
+# weights = NULL
+# na.rm.cat = FALSE
+# na.value.cat = "NA"
+# na.rm.cont = FALSE
+# measure = "phi"
+# limit = NULL
+# correlation = "kendall"
+# robust = TRUE
+# nperm = NULL
+# distrib = "asympt"
+# dec = c(3,3,3,3,1,3)
+
+
 catdesc <- function(y, x, weights = NULL,
                     na.rm.cat = FALSE, na.value.cat = "NA", na.rm.cont = FALSE,
                     measure = "phi", limit = NULL, correlation = "kendall", robust = TRUE,
-                    nperm = NULL, distrib = "asympt", dec = c(3,3,3,3,1,3)) {
+                    nperm = NULL, distrib = "asympt", digits = 2) {
   
   if(is.null(weights)) weights <- rep(1, length(y))
   if(any(is.na(weights))) stop("There are empty values in weights.")
@@ -29,7 +46,7 @@ catdesc <- function(y, x, weights = NULL,
   if(ncol(xcat)>0) {
     lcat <- list()
     for(i in 1:ncol(xcat)) {
-      temp <- assoc.twocat(y, xcat[,i], weights = weights, na.rm = na.rm.cat, na.value = na.value.cat, nperm = NULL)$gather
+      temp <- assoc.twocat(y, xcat[,i], weights = weights, na.rm = na.rm.cat, na.value = na.value.cat, nperm = nperm, distrib = "asympt")$gather
       temp$prop.x <- NULL
       temp$prop.y <- NULL
       temp$categories <- paste(names(xcat)[i],temp$var.y,sep='.')
@@ -38,15 +55,20 @@ catdesc <- function(y, x, weights = NULL,
     lcat <- do.call("rbind.data.frame",lcat)
     lcat <- lcat[order(-lcat[,measure]),]
     if(!is.null(limit)) lcat <- lcat[abs(lcat[,measure])>=limit,]
-    lcat$cprop <- round(lcat$cprop, dec[3])
-    lcat$rprop <- round(lcat$rprop, dec[3])
-    lcat$prop.y <- round(lcat$prop.y, dec[3])
-    lcat[,measure] <- round(lcat[,measure], dec[4])
+    lcat$cprop <- round(100*lcat$cprop, 1)
+    lcat$rprop <- round(100*lcat$rprop, 1)
+    lcat$prop.y <- round(100*lcat$prop.y, 1)
+    lcat[,measure] <- round(lcat[,measure], 3)
     splitvar <- lcat$var.x
-    lcat <- lcat[,c("categories","cprop","rprop","prop.y",measure)]
-    names(lcat) <- c("categories","pct.ycat.in.xcat","pct.xcat.in.ycat","pct.xcat.global",measure)
-    rownames(lcat) <- NULL
+    noms <- c("categories","freq","cprop","rprop","prop.y",measure)
+    if(!is.null(nperm)) {
+      lcat$pvalue <- round(lcat$perm.pval, 5)
+      noms <- c(noms, "pvalue")
+      }
+    lcat <- lcat[, noms]
+    names(lcat)[3:5] <- c("pct.y.in.x","pct.x.in.y","overall.pct.x")
     lcat <- split(lcat, splitvar)
+    lcat <- lapply(lcat, function(x) {rownames(x) <- NULL ; return(x)})
   }
 
   if(ncol(xcon)==0) {
@@ -57,17 +79,16 @@ catdesc <- function(y, x, weights = NULL,
   if(ncol(xcon)>0) {
     lcon <- list()
     for(i in 1:ncol(xcon)) {
-      temp <- data.frame(cor = assoc.catcont(y, xcon[,i], weights = weights,
-                                             na.rm.cat = na.rm.cat, na.value.cat = na.value.cat, na.rm.cont = na.rm.cont,
-                                             nperm = NULL, digits = 9)$cor)
+      asso <- assoc.catcont(y, xcon[,i], weights = weights,
+                            na.rm.cat = na.rm.cat, na.value.cat = na.value.cat, na.rm.cont = na.rm.cont,
+                            nperm = nperm, distrib = distrib)
+      temp <- data.frame(correlation = asso$cor)
+      if(!is.null(nperm)) temp <- data.frame(temp, pvalue = round(asso$cor.perm.pval, 5))
       temp$variables <- rep(names(xcon)[i],nrow(temp))
       temp$categories <- rownames(temp)
       if(robust==TRUE) {
-        # temp$median.x.in.ycat <- sapply(levels(y), function(x) weighted.quantile(xcon[y[!is.na(y)]==x,i], weights[y[!is.na(y)]==x], 
-        #                                                                          na.rm = na.rm.cont, probs = .5))
         temp$median.x.in.ycat <- sapply(split(data.frame(xcon[,i],weights), y), function(x) weighted.quantile(x[,1],x[,2],probs=.5, na.rm = na.rm.cont))
         temp$median.x.global <- rep(weighted.quantile(xcon[,i], weights, na.rm = na.rm.cont, probs = .5), nrow(temp))
-        # temp$mad.x.in.ycat <- sapply(levels(y), function(x) weighted.mad(xcon[y[!is.na(y)]==x,i], weights[y[!is.na(y)]==x], na.rm = na.rm.cont))
         temp$mad.x.in.ycat <- sapply(split(data.frame(xcon[,i],weights), y), function(x) weighted.mad(x[,1],x[,2], na.rm = na.rm.cont))
         temp$mad.x.global <- rep(weighted.mad(xcon[,i], weights, na.rm = na.rm.cont), nrow(temp))
       }
@@ -80,18 +101,28 @@ catdesc <- function(y, x, weights = NULL,
       lcon[[i]] <- temp
     }
     lcon <- do.call("rbind.data.frame",lcon)
-    lcon <- lcon[order(-lcon$cor),]
-    lcon$median.x.in.ycat <- round(lcon$median.x.in.ycat, dec[5])
-    lcon$median.x.global <- round(lcon$median.x.global, dec[5])
-    lcon$mad.x.in.ycat <- round(lcon$mad.x.in.ycat, dec[5])
-    lcon$mad.x.global <- round(lcon$mad.x.global, dec[5])
-    lcon$cor <- round(lcon$cor,dec[6])
+    lcon <- lcon[order(-lcon$correlation),]
+    lcon$median.x.in.ycat <- round(lcon$median.x.in.ycat, digits)
+    lcon$median.x.global <- round(lcon$median.x.global, digits)
+    lcon$mad.x.in.ycat <- round(lcon$mad.x.in.ycat, digits)
+    lcon$mad.x.global <- round(lcon$mad.x.global, digits)
+    # lcon$cor <- round(lcon$cor,dec[6])
     splitvar <- lcon$categories
-    lcon <- lcon[,c(2,4:7,1)]
-    if(robust==FALSE) names(lcon) <- c("variables","mean.x.in.ycat","mean.x.global",
-                                       "sd.x.in.ycat","sd.x.global","cor")
-    rownames(lcon) <- NULL
+    noms <- c("variables", "median.x.in.ycat", "median.x.global", 
+              "mad.x.in.ycat", "mad.x.global", "correlation")
+    if(!is.null(nperm)) noms <- c(noms, "pvalue")
+    lcon <- lcon[, noms]
+    # lcon <- lcon[,c(2,4:7,1)]
+    if(robust==FALSE) {
+      names(lcon)[2:5] <- c("mean.in.category","overall.mean",
+                            "sd.in.category","overall.sd")
+    } else {
+      names(lcon)[2:5] <- c("median.in.category","overall.median",
+                            "mad.in.category","overall.mad")      
+    }
+    # rownames(lcon) <- NULL
     lcon <- split(lcon,splitvar)
+    lcon <- lapply(lcon, function(x) {rownames(x) <- NULL ; return(x)})
   }
   
   bylevel <- list()
@@ -100,35 +131,12 @@ catdesc <- function(y, x, weights = NULL,
     bylevel[[i]]$continuous.var <- lcon[[i]]
   }
   
-  res <- list(variables = assoc.yx(y, x, weights = weights, correlation = correlation,
-                                   na.rm.cat = na.rm.cat, na.value.cat = na.value.cat, na.rm.cont = na.rm.cont,
-                                   xx = FALSE, nperm = nperm, distrib = distrib, dec = dec[1:2])$YX,
+  v <- assoc.yx(y, x, xx = FALSE, weights = weights, correlation = correlation,
+                na.rm.cat = na.rm.cat, na.value.cat = na.value.cat, na.rm.cont = na.rm.cont,
+                nperm = nperm, distrib = distrib, dec = c(3,5))$YX
+  if(!is.null(nperm)) names(v)[4] <- "pvalue"
+  
+  res <- list(variables = v,
               bylevel = bylevel)
   return(res)
 }
-
-# data(Movies)
-# catdesc(Movies$ArtHouse, Movies[,c("Budget","Genre","Country")])
-# catdesc(Movies$ArtHouse, Movies[,c("Budget","Genre","Country")], measure = "pem")
-
-
-# data(Movies)
-# y0 = y1 = Movies$ArtHouse
-# a0 = a1 = Movies$Budget
-# b0 = b1 = Movies$Genre
-# c0 = c1 = Movies$Country
-# w0 = w1 = Movies$Critics
-# w1[1:2] <- NA
-# a1[3:4] <- NA
-# b1[5:6] <- NA
-# c1[7:8] <- NA
-# y1[9:10] <- NA
-# 
-# catdesc(y0, data.frame(a0,b0,c0), weights = w0)
-# catdesc(y0, data.frame(a0,b0,c0), weights = w1)
-# catdesc(y0, data.frame(a1,b0,c0), na.rm.cont = FALSE)
-# catdesc(y0, data.frame(a1,b0,c0), na.rm.cont = TRUE)
-# catdesc(y0, data.frame(a0,b1,c0), na.rm.cat = FALSE, na.value.cat = "99", robust = FALSE)
-# catdesc(y0, data.frame(a0,b1,c0), na.rm.cat = TRUE, na.value.cat = "99", robust = FALSE)
-# catdesc(y1, data.frame(a0,b0,c0), na.rm.cat = FALSE, na.value.cat = "99", robust = FALSE)
-# catdesc(y1, data.frame(a0,b0,c0), na.rm.cat = TRUE, na.value.cat = "99", robust = FALSE)
